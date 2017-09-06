4e75129e50bc452605a5439bbaf428e1dc10b258
Unknown <benjaminr@laposte.net>
2017-09-07 00:50:32 +0200

    Matchmaking test

---
+++ Qin/Assets/Scripts/SC_Matchmaker.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+using UnityEngine.Networking.Match;
+using UnityEngine.Networking.Types;
+
+public class SC_Matchmaker : MonoBehaviour {
+
+	public SC_Menu menuManager;
+	bool selectedSide;
+	ulong? createdMatchID;
+
+	void Start() {
+
+		selectedSide = false;
+		createdMatchID = null;
+
+	}
+
+	public void QuickMatchmaking(bool qin) {
+
+
+
+		NetworkManager.singleton.StartMatchMaker ();
+		NetworkManager.singleton.matchMaker.ListMatches (0, 10, qin ? "Heroes" : "Qin", true, 0, 0, OnMatchList); 
+		selectedSide = qin;
+
+	}
+
+	public void OnMatchList(bool success, string extendedInfo, List<MatchInfoSnapshot> matches) {
+
+		if (matches.Count == 0) {
+
+			if (createdMatchID == null) {
+
+				createdMatchID = 0;
+				NetworkManager.singleton.matchMaker.CreateMatch ("Qin", 2, true, "", "", "", 0, 0, OnMatchCreate);
+
+			}
+
+			QuickMatchmaking (selectedSide);
+
+		} else {
+
+			NetworkManager.singleton.matchMaker.JoinMatch (matches [0].networkId, "", "", "", 0, 0, OnMatchJoined);
+
+		}
+
+	}
+
+	public void OnMatchCreate(bool success, string extendedInfo, MatchInfo matchInfo) {
+
+		createdMatchID = (ulong?) matchInfo.networkId;
+
+	}
+
+	public void OnMatchJoined(bool success, string extendedInfo, MatchInfo matchInfo) {
+
+		if (((ulong?)matchInfo.networkId) != createdMatchID)
+			NetworkManager.singleton.matchMaker.DestroyMatch ((NetworkID)createdMatchID, 0, OnMatchDestroy);
+
+	}
+
+	public void OnMatchDestroy(bool success, string extendedInfo) { }
+
+}
---
+++ Qin/Assets/Scripts/SC_Menu.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SC_Menu : MonoBehaviour {
+
+	public GameObject mainPanel, onlinePanel, qmPanel, searchGamePanel;
+
+	void Awake() {
+
+		mainPanel.SetActive (true);
+
+	}
+
+	void Update() {
+
+		if (Input.GetKeyDown (KeyCode.Escape))
+			GameObject.Find ("Back_Button").GetComponent<Button> ().onClick.Invoke ();
+
+	}
+
+	public void OnlineGame() {
+
+		mainPanel.SetActive (false);
+		qmPanel.SetActive (false);
+		onlinePanel.SetActive (true);
+
+	}
+
+	public void QuickMatchmaking() {
+
+		onlinePanel.SetActive (false);
+		qmPanel.SetActive (true);
+
+	}
+
+	public void SearchingGame() {
+
+		qmPanel.SetActive (false);
+		searchGamePanel.SetActive (true);
+
+	}
+
+	public void Back(string panel) {
+
+		foreach (Transform t in transform)
+			t.gameObject.SetActive (t.name.Equals (panel));
+
+	}
+
+}
