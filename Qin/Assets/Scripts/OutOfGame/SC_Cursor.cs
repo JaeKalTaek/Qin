ac9c950a50e22f80ff6d0a0be5110fbf452d6526
Florent Vieillescazes <florent.vieillescazes@hotmail.fr>
2018-09-19 23:53:20 +0200

    Beginning of Camerea/Cursor

---
+++ Qin/Assets/Scripts/OutOfGame/SC_Cursor.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SC_Cursor : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+
+    {
+
+        transform.SetPos(new Vector3((int)Input.mousePosition.x, (int)Input.mousePosition.y, 0));
+
+        if (Input.GetAxis("Fire1") != 0)
+            SC_Tile_Manager.Instance?.GetTileAt(transform.position).CursorClick();
+
+    }
+
+    void Test()
+    {
+
+        
+
+    }
+}
